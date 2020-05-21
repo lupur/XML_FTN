9b0613b786439761a0d60028d2cb3cbe681daaec
Sava Maric <sava.maric@cyrillicsoftware.net>
2020-05-21 22:16:24 +0200

    Adding necessary dependencies to microservices

---
+++ backend/services/soap-service/src/test/java/com/ftnxml/soapservice/SoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftnxml.soapservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
