76445bf0a44235da8721bbe6989db4f40c54482e
mlupurovic <mlupurovic@execom.eu>
2020-05-21 22:06:47 +0200

    Added OrderProcessing and VehicleManagament skeletons

---
+++ backend/services/vehicle-managament/src/main/java/com/ftnxml/vehiclemanagament/VehicleManagamentApplication.java
@@ -0,0 +1,13 @@
+package com.ftnxml.vehiclemanagament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleManagamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleManagamentApplication.class, args);
+	}
+
+}
---
+++ backend/services/vehicle-managament/src/test/java/com/ftnxml/vehiclemanagament/VehicleManagamentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftnxml.vehiclemanagament;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleManagamentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
