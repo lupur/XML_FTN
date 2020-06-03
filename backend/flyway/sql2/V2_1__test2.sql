10706d7f27890f672415bd75efdae60f6d657e73
Sava Maric <sava.maric@cyrillicsoftware.net>
2020-06-03 22:09:45 +0200

    User service tables

--- backend/flyway/sql2/V2_1__test2.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE test2.jasa_sa_salasa (
-	`jasa_id` bigint(20) NOT NULL,
-    PRIMARY KEY (jasa_id)
-)
---
+++ backend/flyway/user_service/V2_1__user_service.sql
@@ -0,0 +1,25 @@
+CREATE TABLE user_service.role (
+  `role_id` bigint(20) NOT NULL,
+  `role_name` varchar(48) NOT NULL,
+  PRIMARY KEY (`role_id`)
+);
+
+CREATE TABLE user_service.user (
+	`user_id` bigint(20) NOT NULL,
+    `user_name` varchar(48) NOT NULL,
+    `password` varchar(48) NOT NULL,
+    `email` varchar(48) NOT NULL,
+    `register_date` date NOT NULL,
+    `account_status` enum('INACTIVE', 'ACTIVE', 'BLOCKED'),
+    PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE user_service.user_role (
+	`user_role_id` bigint(20) NOT NULL,
+    `user_id` bigint(20) NOT NULL,
+    `role_id` bigint(20) NOT NULL,
+    PRIMARY KEY (`user_role_id`),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user_service.user(user_id),
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES user_service.role(role_id),
+	UNIQUE KEY id_user_role (`user_id`, `role_id`)
+);
