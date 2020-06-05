89cb477389389662a8abd4d9167cda609d8adc4c
Sava Maric <sava.maric@cyrillicsoftware.net>
2020-06-05 21:49:47 +0200

    Order-processing fixing

--- backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
+++ backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/order")
 public class OrderProcessingController {
 	
 	public class VehicleController {
