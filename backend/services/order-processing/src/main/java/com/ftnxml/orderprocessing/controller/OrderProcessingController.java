b81bc263453c1824fe922e7768d067d8ada3b4de
Sava Maric <sava.maric@cyrillicsoftware.net>
2020-06-05 23:13:26 +0200

    Controller replacement

--- backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
+++ backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
@@ -1,18 +1,20 @@
 package com.ftnxml.orderprocessing.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/order")
 public class OrderProcessingController {
 	
 	public class VehicleController {
 
-	    @GetMapping("/")
-	    public String home() {
-	        return "Hello from Order-Processing Service";
+	    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	    public ResponseEntity getBrands() {
+	        return ResponseEntity.ok(new ArrayList<>());
 	    }
 	}
 
