3b56d1ac5a96f74d6e566ff952e77b89a7110b37
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-13 23:55:57 +0200

    implement role mapping

---
+++ agent/CarRentalPortal.API/Controllers/UserRolesController.cs
@@ -0,0 +1,16 @@
+﻿using CarRentalPortal.API.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CarRentalPortal.API.Controllers
+{
+    public class UserRolesController : ApiController
+    {
+        [HttpPost, Authorize(Roles = Roles.Administrator)]
+        public async Task<(int, int)> Create(CreateUserRoleCommand command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
