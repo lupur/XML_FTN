4d08b50b1eb94196e64f1e887893dfaae4b7c815
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-14 16:55:25 +0200

    implement car image upload

--- agent/CarRentalPortal.API/Controllers/CarAdsController.cs
+++ agent/CarRentalPortal.API/Controllers/CarAdsController.cs
@@ -1,8 +1,13 @@
 ﻿using CarRentalPortal.API.Constants;
 using CarRentalPortal.Application.CarAds.Commands.CreateCarAd;
 using CarRentalPortal.Application.CarAds.Queries.GetCarAds;
+using CarRentalPortal.Application.CarImages.Commands.UploadCarImage;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace CarRentalPortal.API.Controllers
@@ -20,6 +25,52 @@ namespace CarRentalPortal.API.Controllers
         public async Task<ActionResult<int>> Create(CreateCarAdCommand command)
         {
             return await Mediator.Send(command);
+        }
+
+        [HttpPost("{carAdId}/images"), DisableRequestSizeLimit]
+        public async Task<ActionResult<string>> Upload([FromRoute] int carAdId)
+        {
+            try
+            {
+                var resourcePath = Path.Combine("Resources", "Images", "Cars", $"{carAdId}");
+
+                var imagePath = CopyImage(Request.Form.Files[0], resourcePath);
+
+                var command = new UploadCarImageCommand
+                {
+                    CarAdId = carAdId,
+                    ImagePath = imagePath
+                };
+                return await Mediator.Send(command);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        private string CopyImage(IFormFile file, string resourcePath)
+        {
+            string imagePath = null;
+            var destinationPath = Path.Combine(Directory.GetCurrentDirectory(), resourcePath);
+
+            if (file.Length > 0)
+            {
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                var fullPath = Path.Combine(destinationPath, fileName);
+                imagePath = Path.Combine(resourcePath, fileName);
+
+                if (!Directory.Exists(resourcePath))
+                {
+                    Directory.CreateDirectory(resourcePath);
+                }
+
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+            }
+            return imagePath;
         }
     }
 }
---
+++ agent/CarRentalPortal.Application/CarImages/Commands/UploadCarImage/UploadCarImageCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using CarRentalPortal.Application.Common.Interfaces;
+using CarRentalPortal.Core.Entities;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CarRentalPortal.Application.CarImages.Commands.UploadCarImage
+{
+    public class UploadCarImageCommandHandler : IRequestHandler<UploadCarImageCommand, string>
+    {
+        private readonly IApplicationDbContext _appContext;
+
+        public UploadCarImageCommandHandler(IApplicationDbContext appContext)
+        {
+            _appContext = appContext;
+        }
+
+        public async Task<string> Handle(UploadCarImageCommand request, CancellationToken cancellationToken)
+        {
+            var entity = new CarImage
+            {
+                CarAdId = request.CarAdId,
+                Uri = request.ImagePath
+            };
+
+            _appContext.CarImages.Add(entity);
+            await _appContext.SaveChangesAsync(cancellationToken);
+
+            return entity.Uri;
+        }
+    }
+}
