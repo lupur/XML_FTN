f28d4158804d63991ad330d5f87ba0886f849306
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-15 21:56:05 +0200

    update car category

--- agent/CarRentalPortal.Core/Entities/CarCategory.cs
+++ agent/CarRentalPortal.Core/Entities/CarCategory.cs
@@ -10,6 +10,7 @@ namespace CarRentalPortal.Core.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
+        public double RentalValue { get; set; }
         public ICollection<Car> Cars { get; set; }
     }
 }
