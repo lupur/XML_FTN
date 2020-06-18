caa9403c59d02522786ae8288716bd1c37789f5e
mlupurovic <mlupurovic@execom.eu>
2020-06-18 13:54:38 +0200

    Kafka broker setup

--- backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
+++ backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/controller/OrderProcessingController.java
@@ -19,131 +19,131 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ftnxml.orderprocessing.dto.OrderRequestDto;
 import com.ftnxml.orderprocessing.dto.OrderRequestMapper;
-import com.ftnxml.orderprocessing.messaging.OrderRequestPublish;
 import com.ftnxml.orderprocessing.enums.OrderRequestStatus;
+import com.ftnxml.orderprocessing.messaging.OrderRequestPublish;
 import com.ftnxml.orderprocessing.service.OrderRequestService;
 
 @RestController
 @RequestMapping("/")
 public class OrderProcessingController {
 
-	@Autowired
-	OrderRequestService orderRequestService;
-	
-	@Autowired
-	OrderRequestPublish orderRequestPublish;
-	
-	@PostMapping(value = "/publish")
-	public ResponseEntity publish() {
-		List<Long> publishContent = Arrays.asList(1l, 2l, 3l);
-		orderRequestPublish.sendOrderRequest(publishContent);
-		return ResponseEntity.ok().build();
-	}
-	
-	// GET ALL
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getRequests() {
-		List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
-		        .toOrderRequestDTOs(orderRequestService.getAllOrderRequests());
-		if (orderRequests != null) {
-			return ResponseEntity.ok(orderRequests);
-		}
-		return ResponseEntity.badRequest().build();
-	}
+    @Autowired
+    OrderRequestService orderRequestService;
 
-	// GET BY REQUEST
-	@GetMapping(value = "/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getRequestsByRequestId(@PathParam("requestId") Long requestId) {
-		OrderRequestDto orderRequest = OrderRequestMapper.INSTANCE
-		        .toOrderRequestDTO(orderRequestService.getOrderRequest(requestId));
-		if (orderRequest != null) {
-			return ResponseEntity.ok(orderRequest);
-		}
-		return ResponseEntity.badRequest().build();
-	}
+    @Autowired
+    OrderRequestPublish orderRequestPublish;
 
-	// GET BY VEHICLE
-	@GetMapping(value = "/vehicles/{vehicleId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getRequestsByVehicleId(@PathParam("vehicleId") Long vehicleId) {
-		List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
-		        .toOrderRequestDTOs(orderRequestService.getOrderRequestByVehicleId(vehicleId));
-		if (orderRequests != null) {
-			return ResponseEntity.ok(orderRequests);
-		}
-		return ResponseEntity.badRequest().build();
-	}
+    @PostMapping(value = "/publish")
+    public ResponseEntity publish() {
+        List<Long> publishContent = Arrays.asList(1l, 2l, 3l);
+        orderRequestPublish.sendOrderRequest(publishContent);
+        return ResponseEntity.ok().build();
+    }
 
-	// GET BY USER
-	@GetMapping(value = "/users/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getRequestsByUserId(Long vehicleId) {
-		List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
-		        .toOrderRequestDTOs(orderRequestService.getOrderRequestByUserId(vehicleId));
-		if (orderRequests != null) {
-			return ResponseEntity.ok(orderRequests);
-		}
-		return ResponseEntity.badRequest().build();
-	}
+    // GET ALL
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRequests() {
+        List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
+                .toOrderRequestDTOs(orderRequestService.getAllOrderRequests());
+        if (orderRequests != null) {
+            return ResponseEntity.ok(orderRequests);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-	// GET BY OWNER
-	@GetMapping(value = "/owners/{ownerId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity getRequestsByOwnerId(@PathParam("ownerId") Long vehicleId) {
-		List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
-		        .toOrderRequestDTOs(orderRequestService.getOrderRequestByOwnerId(vehicleId));
-		if (orderRequests != null) {
-			return ResponseEntity.ok(orderRequests);
-		}
-		return ResponseEntity.badRequest().build();
-	}
+    // GET BY REQUEST
+    @GetMapping(value = "/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRequestsByRequestId(@PathParam("requestId") Long requestId) {
+        OrderRequestDto orderRequest = OrderRequestMapper.INSTANCE
+                .toOrderRequestDTO(orderRequestService.getOrderRequest(requestId));
+        if (orderRequest != null) {
+            return ResponseEntity.ok(orderRequest);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-	// CRETE NEW REQUEST
-	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity addOrderRequest(@RequestBody OrderRequestDto orderRequestDto) {
-		if (orderRequestDto == null) {
-			System.out.println("OrderRequestBody is not defined");
-			return ResponseEntity.badRequest().build();
-		}
-		try {
-			if (orderRequestService
-			        .addOrderRequest(OrderRequestMapper.INSTANCE.INSTANCE.toOrderRequest(orderRequestDto)))
-				return ResponseEntity.ok().build();
-			else
-				return ResponseEntity.badRequest().build();
-		} catch (Exception e) {
-			e.printStackTrace();
-			return ResponseEntity.badRequest().build();
-		}
-	}
+    // GET BY VEHICLE
+    @GetMapping(value = "/vehicles/{vehicleId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRequestsByVehicleId(@PathParam("vehicleId") Long vehicleId) {
+        List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
+                .toOrderRequestDTOs(orderRequestService.getOrderRequestByVehicleId(vehicleId));
+        if (orderRequests != null) {
+            return ResponseEntity.ok(orderRequests);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-	// DELETE REQUEST
-	@DeleteMapping(value = "/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity removeOrderRequest(@PathVariable Long requestId) {
-		if (orderRequestService.removeOrderRequest(requestId))
-			return ResponseEntity.ok().build();
-		else
-			return ResponseEntity.notFound().build();
-	}
+    // GET BY USER
+    @GetMapping(value = "/users/{userId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRequestsByUserId(Long vehicleId) {
+        List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
+                .toOrderRequestDTOs(orderRequestService.getOrderRequestByUserId(vehicleId));
+        if (orderRequests != null) {
+            return ResponseEntity.ok(orderRequests);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-	@PutMapping(value = "/{requestId}/reserved", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity setOrderRequestReserved(@PathVariable Long requestId) {
-		if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.RESREVED))
-			return ResponseEntity.ok("Order request reserved.");
-		else
-			return ResponseEntity.badRequest().body("No order request with given id");
-	}
+    // GET BY OWNER
+    @GetMapping(value = "/owners/{ownerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity getRequestsByOwnerId(@PathParam("ownerId") Long vehicleId) {
+        List<OrderRequestDto> orderRequests = OrderRequestMapper.INSTANCE
+                .toOrderRequestDTOs(orderRequestService.getOrderRequestByOwnerId(vehicleId));
+        if (orderRequests != null) {
+            return ResponseEntity.ok(orderRequests);
+        }
+        return ResponseEntity.badRequest().build();
+    }
 
-	@PutMapping(value = "/{requestId}/paid", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity setOrderRequestPaid(@PathVariable Long requestId) {
-		if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.PAID))
-			return ResponseEntity.ok("Order request paid.");
-		else
-			return ResponseEntity.badRequest().body("No order request with given id");
-	}
+    // CRETE NEW REQUEST
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity addOrderRequest(@RequestBody OrderRequestDto orderRequestDto) {
+        if (orderRequestDto == null) {
+            System.out.println("OrderRequestBody is not defined");
+            return ResponseEntity.badRequest().build();
+        }
+        try {
+            if (orderRequestService
+                    .addOrderRequest(OrderRequestMapper.INSTANCE.INSTANCE.toOrderRequest(orderRequestDto)))
+                return ResponseEntity.ok().build();
+            else
+                return ResponseEntity.badRequest().build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+    }
 
-	@PutMapping(value = "/{requestId}/canceled", produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity setOrderRequestCanceled(@PathVariable Long requestId) {
-		if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.CANCELED))
-			return ResponseEntity.ok("Order request canceled.");
-		else
-			return ResponseEntity.badRequest().body("No order request with given id");
-	}
+    // DELETE REQUEST
+    @DeleteMapping(value = "/{requestId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity removeOrderRequest(@PathVariable Long requestId) {
+        if (orderRequestService.removeOrderRequest(requestId))
+            return ResponseEntity.ok().build();
+        else
+            return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping(value = "/{requestId}/reserved", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity setOrderRequestReserved(@PathVariable Long requestId) {
+        if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.RESREVED))
+            return ResponseEntity.ok("Order request reserved.");
+        else
+            return ResponseEntity.badRequest().body("No order request with given id");
+    }
+
+    @PutMapping(value = "/{requestId}/paid", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity setOrderRequestPaid(@PathVariable Long requestId) {
+        if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.PAID))
+            return ResponseEntity.ok("Order request paid.");
+        else
+            return ResponseEntity.badRequest().body("No order request with given id");
+    }
+
+    @PutMapping(value = "/{requestId}/canceled", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity setOrderRequestCanceled(@PathVariable Long requestId) {
+        if (orderRequestService.changeOrderRequestStatus(requestId, OrderRequestStatus.CANCELED))
+            return ResponseEntity.ok("Order request canceled.");
+        else
+            return ResponseEntity.badRequest().body("No order request with given id");
+    }
 }
