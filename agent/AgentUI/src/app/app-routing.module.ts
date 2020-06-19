41ed66245d2a414ee4e67325f46991708cc459e7
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-19 15:47:01 +0200

    create basic login and register components

--- agent/AgentUI/src/app/app-routing.module.ts
+++ agent/AgentUI/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
