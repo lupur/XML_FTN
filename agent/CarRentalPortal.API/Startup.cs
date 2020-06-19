158e58a5c247b9df87d0bb0ca7e64a52efc7f6c8
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-19 18:32:58 +0200

    clean startup cs

--- agent/CarRentalPortal.API/Startup.cs
+++ agent/CarRentalPortal.API/Startup.cs
@@ -22,7 +22,6 @@ namespace CarRentalPortal.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication();
@@ -38,7 +37,6 @@ namespace CarRentalPortal.API
                 options.Filters.Add(new ApiExceptionFilter()));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
