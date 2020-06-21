2c3c3ed2cca5693da3e946a2399334f1891b8393
Sava Maric <sava.maric@cyrillicsoftware.net>
2020-06-21 20:11:42 +0200

    Vehicles with no orders are always available

--- backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/service/VehicleOrderServiceImpl.java
+++ backend/services/order-processing/src/main/java/com/ftnxml/orderprocessing/service/VehicleOrderServiceImpl.java
@@ -6,7 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ftnxml.orderprocessing.model.OrderRequest;
 import com.ftnxml.orderprocessing.model.VehicleOrder;
+import com.ftnxml.orderprocessing.repository.OrderRequestRepository;
 import com.ftnxml.orderprocessing.repository.VehicleOrderRepository;
 
 @Service
@@ -14,6 +16,9 @@ public class VehicleOrderServiceImpl implements VehicleOrderService {
 
 	@Autowired
 	VehicleOrderRepository vehicleOrderRepository;
+	
+	@Autowired
+	OrderRequestRepository orderRequestRepository;
 	
 	@Override
 	public List<VehicleOrder> getAllVehicleOrders() {
@@ -59,7 +64,12 @@ public class VehicleOrderServiceImpl implements VehicleOrderService {
 	@Override
 	public List<Long> findVehiclesAvailableInRange(Date startDate, Date endDate, List<Long> vehicleIDs) {
 		try {
-			return vehicleOrderRepository.findVehiclesAvailableInRange(startDate, endDate, vehicleIDs);
+			List<Long> vehicleIDsResponse =  vehicleOrderRepository.findVehiclesAvailableInRange(startDate, endDate, vehicleIDs);
+			for(Long vehicleID : vehicleIDs) {
+				List<OrderRequest> oReqs = orderRequestRepository.findByVehicleId(vehicleID);
+				if(oReqs == null || oReqs.isEmpty()) vehicleIDsResponse.add(vehicleID);
+			}
+			return vehicleIDsResponse;
 		} catch(Exception e) {
 			e.printStackTrace();
 			return null;
