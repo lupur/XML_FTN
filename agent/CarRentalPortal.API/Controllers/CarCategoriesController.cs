6735142410e40e04a7078821d08df391f4a0ffa3
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-06-27 15:20:25 +0200

    delete and get single car category

--- agent/CarRentalPortal.API/Controllers/CarCategoriesController.cs
+++ agent/CarRentalPortal.API/Controllers/CarCategoriesController.cs
@@ -1,6 +1,8 @@
 ﻿using CarRentalPortal.API.Constants;
 using CarRentalPortal.Application.CarCategories.Commands.CreateCarCategory;
+using CarRentalPortal.Application.CarCategories.Commands.DeleteCarCategory;
 using CarRentalPortal.Application.CarCategories.Queries.GetCarCategories;
+using CarRentalPortal.Application.CarCategories.Queries.GetCarCategory;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -20,6 +22,20 @@ namespace CarRentalPortal.API.Controllers
         public async Task<ActionResult<CarCategoryVm>> Get()
         {
             return await Mediator.Send(new GetCarCategoriesQuery());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CarCategoryDto>> Get(int id)
+        {
+            return await Mediator.Send(new GetCarCategoryQuery { Id = id });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await Mediator.Send(new DeleteCarCategoryCommand { Id = id });
+
+            return NoContent();
         }
     }
 }
