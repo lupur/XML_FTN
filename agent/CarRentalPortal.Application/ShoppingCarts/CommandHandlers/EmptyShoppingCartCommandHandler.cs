6d44d63bbe42f06b70c8a5e89391c296005c70e8
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-07-10 20:03:28 +0200

    fix bulk remove

--- agent/CarRentalPortal.Application/ShoppingCarts/CommandHandlers/EmptyShoppingCartCommandHandler.cs
+++ agent/CarRentalPortal.Application/ShoppingCarts/CommandHandlers/EmptyShoppingCartCommandHandler.cs
@@ -3,6 +3,7 @@ using CarRentalPortal.Application._Common.Interfaces;
 using CarRentalPortal.Application.ShoppingCarts.Commands;
 using CarRentalPortal.Core.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -19,7 +20,7 @@ namespace CarRentalPortal.Application.ShoppingCarts.CommandHandlers
 
         public async Task<Unit> Handle(EmptyShoppingCartCommand request, CancellationToken cancellationToken)
         {
-            var shoppingCart = await _context.ShoppingCarts.FindAsync(request.Id);
+            var shoppingCart = await _context.ShoppingCarts.Include(sc => sc.Items).FirstOrDefaultAsync(sc => sc.Id == request.Id);
             if (shoppingCart == null)
                 throw new NotFoundException(nameof(ShoppingCart), request.Id);
 
