d45dcfa7e5c37806ef87a7feff5b5e67478ce586
Dušan Nikolić <thenikolicdusan@gmail.com>
2020-07-11 12:10:36 +0200

    implement cart update backend

---
+++ agent/CarRentalPortal.Application/ShoppingCarts/Models/UpdateShoppingCartItemModel.cs
@@ -0,0 +1,10 @@
+﻿using CarRentalPortal.Core.Enums;
+
+namespace CarRentalPortal.Application.ShoppingCarts.Models
+{
+    public class UpdateShoppingCartItemModel
+    {
+        public int Id { get; set; }
+        public OrderStatus Status { get; set; }
+    }
+}
